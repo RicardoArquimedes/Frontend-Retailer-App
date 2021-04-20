d7c5d20e9f7a45c731b14c31a365c5bdc111f386
RicardoArquimedes <richardcqt28@gmail.com>
2021-04-20 09:48:28 -0500

    updating forms components

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,19 +1,17 @@
 import React, { useState } from 'react';
+
 import axios from "../Utils/axios";
 
 
 function Orders(props) {
   const [loading, setLoading] = useState(false);
-  const subtotal = formUseInput('');
-  const total = formUseInput('');
+  const subtotal = useInputFormData('');
+  const total = useInputFormData('');
 
 
   const [error, setError] = useState(null);
 
-
-
-  // handle button click of login form
-  const handleLogin = async () => {
+  const handleOrders = async () => {
 
     setError(null);
     setLoading(true);
@@ -25,6 +23,7 @@ function Orders(props) {
       console.log("todo", resProfile)
       await axios.post('/api/orders/', order)
       alert("order registered succesfully")
+      // props.history.push('/');
       setLoading(false);
     } catch (e) {
       console.log(e)
@@ -37,38 +36,40 @@ function Orders(props) {
   }
 
   return (
+
     <div>
-    <div className="flex flex-row font-sans container mx-auto h-full  justify-center items-center mt-20">
-     
-   
-      <div className="bg-blue-500 text-white shadow-md rounded px-20 pt-10 pb-12 mb-4">
-        <div>
-        <h1 className="mb-10 font-bold">Orders </h1>
+      <div className="flex flex-row font-sans container mx-auto h-full  justify-center items-center mt-20">
+
+
+        <div className="bg-blue-500 text-white shadow-md rounded px-20 pt-10 pb-12 mb-4">
+          <div>
+            <h1 className="mb-10 font-bold">Orders </h1>
+          </div>
+          <div>
+            <label className="block text-grey-darker text-md font-bold mb-2" htmlFor='subtotal'>Total</label>
+            <input className="form-control text-gray-900" type="text" name="total" {...total} />
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <label className="block text-grey-darker text-md font-bold mb-2" htmlFor='total'>Subtotal</label>
+            <input className="form-control text-gray-900" type="text" class name="subtotal" {...subtotal} />
+          </div>
+          {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+          <input className="bg-white text-blue-500 font-bold py-2 px-4 mt-10 rounded" type="button" value={loading ? 'Loading...' : 'Submit Order'} onClick={handleOrders} disabled={loading} /><br />
         </div>
-        <div>
-          <label className="block text-grey-darker text-md font-bold mb-2" htmlFor='subtotal'>Total</label>
-          <input className="form-control text-gray-900" type="text" name="total" {...total} />
-        </div>
-        <div style={{ marginTop: 10 }}>
-          <label className="block text-grey-darker text-md font-bold mb-2" htmlFor='total'>Subtotal</label>
-          <input  className="form-control text-gray-900"type="text"class name="subtotal" {...subtotal} />
-        </div>
-          <input className="bg-white text-blue-500 font-bold py-2 px-4 mt-10 rounded" type="button" value={loading ? 'Loading...' : 'Submit Order'} onClick={handleLogin} disabled={loading} /><br />
       </div>
-    </div>
     </div>
   );
 }
 
-const formUseInput = initialValue => {
+const useInputFormData = initialValue => {
   const [value, setValue] = useState(initialValue);
 
-  const inputHandleChange = e => {
+  const handleOrdersChange = e => {
     setValue(e.target.value);
   }
   return {
     value,
-    onChange: inputHandleChange
+    onChange: handleOrdersChange
   }
 }
 
